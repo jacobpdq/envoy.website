01535dba35bf83b5503ee0c4ae0eea3d661a11ba
Jacob Friedman <jacob@subsumo.com>
2015-07-08 13:14:28 -0400

    lang switch removed from bootstrap

--- envoynet/config/bootstrap.php
+++ envoynet/config/bootstrap.php
@@ -226,20 +226,7 @@ Type::build('datetime')->useLocaleParser();
 if (isset($_POST["language"])) {
 
  switch($_POST['language']) {
-      case "en":
-      I18n::locale('fr_CA');
-      $_POST['language'] = 'fr';
-      break;
-      case "fr":
-      I18n::locale('en_CA');
-      $_POST['language'] = 'en';
-      break;
-      default:
-      I18n::locale('en_CA');
-      $_POST['language'] == "en";
 
-      break;   
-    }
 } else {
     $_POST["language"] = "en";
 }
