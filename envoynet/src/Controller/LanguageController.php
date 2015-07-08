c9522479971efc6d390dd865e2d137664754f04e
Jacob Friedman <jacob@subsumo.com>
2015-07-08 11:18:34 -0400

    language v3

--- envoynet/src/Controller/LanguageController.php
+++ envoynet/src/Controller/LanguageController.php
@@ -3,21 +3,18 @@ namespace App\Controller;
 
 use App\Controller\AppController;
 
-class LanguageController extends AppController
+class MainController extends AppController
 {
+    	public function language($language = null) {
 
-	  public function language($language = null) {
-
-    switch($language) {
-      case "en":
-      I18n::locale('en_CA');
-      break;
-      case "fr":
-      I18n::locale('fr_CA');
-      default:
-      I18n::locale('en_CA');    
-    }
-
-  }
-
+		switch($language) {
+			case "en":
+			I18n::locale('en_CA');
+			break;
+			case "fr":
+			I18n::locale('fr_CA');
+			default:
+			I18n::locale('en_CA');    
+		}
+	}
 }
