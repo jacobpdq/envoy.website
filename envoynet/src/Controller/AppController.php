80dc632e17d4b781cb2e1ada8b100468e17862ad
Jacob Friedman <jacob@subsumo.com>
2015-07-08 14:06:13 -0400

    params

--- envoynet/src/Controller/AppController.php
+++ envoynet/src/Controller/AppController.php
@@ -80,7 +80,7 @@ class AppController extends Controller {
         $this->request->session()->write('language', 'en');
     }
     echo '<pre>';
-    var_dump($this->request->params['pass']);
+    var_dump($this->request->params);
     echo '</pre>';
     if (isset($this->request['prefix'])) {
       if ($this->request['prefix'] == 'admin') {
