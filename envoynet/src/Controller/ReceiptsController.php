aef25b8827e114ddd48fbc8d37ecd58ce8065be2
Jacob Friedman <jacob@subsumo.com>
2015-07-09 15:22:47 -0400

    receipts

--- envoynet/src/Controller/ReceiptsController.php
+++ envoynet/src/Controller/ReceiptsController.php
@@ -7,6 +7,8 @@ use App\Controller\AppController;
 class ReceiptsController extends AppController {
 
   public $name = 'Receipts';
+   $this->set('title_for_layout', __('Receipts'));
+
   
   public $paginate = [];
 
