3ea44a071c2add9f16c0b6ed6f5af77dffbcfb7b
Jacob Friedman <jacob@subsumo.com>
2015-07-10 11:00:43 -0400

    action update

--- envoynet/src/Controller/MainController.php
+++ envoynet/src/Controller/MainController.php
@@ -27,7 +27,7 @@ class MainController extends AppController {
   public function beforeFilter(Event $event) {
     parent::beforeFilter($event);
     $this->Auth->allow(array('contact', 'index','login','logout'));
-    $this->Auth->allowedActions = array('forgot');
+    $this->Auth->allowedActions = array('forgotAgentPassword','forgotSupplierPassword');
 
   }
 
