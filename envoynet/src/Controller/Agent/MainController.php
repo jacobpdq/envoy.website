a3a57d49843145362dc25a109b9ec65bf7b72dfd
Jacob Friedman <jacob@subsumo.com>
2015-07-10 10:09:31 -0400

    agentpwd

--- envoynet/src/Controller/Agent/MainController.php
+++ envoynet/src/Controller/Agent/MainController.php
@@ -22,7 +22,7 @@ class MainController extends \App\Controller\MainController {
     parent::initialize();
   }
   public function beforeFilter(Event $event) {
-    pritn_r($event);
+    print_r($event);
     parent::beforeFilter($event);
     $this->Auth->deny('index');
   }
