f47b03f5efec3b24a7085178403f93dda76f4c39
Jacob Friedman <jacob@subsumo.com>
2015-07-10 10:18:32 -0400

    pwd forgot

--- envoynet/src/Controller/Agent/MainController.php
+++ envoynet/src/Controller/Agent/MainController.php
@@ -24,8 +24,8 @@ class MainController extends \App\Controller\MainController {
   public function beforeFilter(Event $event) {
     echo $this->request->params['action'];
 
-    parent::beforeFilter($event);
-    $this->Auth->deny('index');
+    //parent::beforeFilter($event);
+    //$this->Auth->deny('index');
   }
 
   public function login() {
