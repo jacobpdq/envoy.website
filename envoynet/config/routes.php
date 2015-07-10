d0dcfc97e1f4f6a709434c9f6ceee33c091ac5ba
Jacob Friedman <jacob@subsumo.com>
2015-07-10 09:43:13 -0400

    password

--- envoynet/config/routes.php
+++ envoynet/config/routes.php
@@ -50,11 +50,9 @@ Router::scope('/', function ($routes) {
     $routes->connect('sso-login/*',['controller'=>'Main','action'=>'login','prefix'=>'agent']);
     $routes->connect('sso-logout/*',['controller'=>'Main','action'=>'logout','prefix'=>'agent']);
 
-    Router::prefix('password', function ($routes) {
-        // All routes here will be prefixed with `/agent`
-        // And have the prefix => agent route element added.
-        $routes->connect('/forgot', array('controller' => 'Main', 'action' => 'passwordForgot'));
-    });
+     $routes->connect('/password/forgot', array('controller' => 'Main', 'action' => 'passwordForgot'));
+   
+
 
     Router::prefix('agent', function ($routes) {
         // All routes here will be prefixed with `/agent`
