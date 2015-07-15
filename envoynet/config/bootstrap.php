0142a81c6491675df0b70db438a5c55035a307ea
Jacob Friedman <jacob@subsumo.com>
2015-07-15 11:34:48 -0400

    clear cache 2

--- envoynet/config/bootstrap.php
+++ envoynet/config/bootstrap.php
@@ -205,12 +205,16 @@ Request::addDetector('tablet', function ($request) {
 //Plugin::load('Migrations');
 Plugin::loadAll(); 
 
+Configure::write('debug', 2); // It was 0
+
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
     //Plugin::load('DebugKit', ['bootstrap' => true]);
     Configure::write('debug', 2); // It was 0
 }
+
+Cache::disable();
 
 /**
  * Connect middleware/dispatcher filters.
