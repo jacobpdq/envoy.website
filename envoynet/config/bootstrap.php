ef97bb4aa373612f1910408bfc0942996cd2a0c6
Jacob Friedman <jacob@subsumo.com>
2015-07-15 11:33:03 -0400

    debug

--- envoynet/config/bootstrap.php
+++ envoynet/config/bootstrap.php
@@ -209,6 +209,7 @@ Plugin::loadAll();
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
     //Plugin::load('DebugKit', ['bootstrap' => true]);
+    Configure::write('debug', 2); // It was 0
 }
 
 /**
