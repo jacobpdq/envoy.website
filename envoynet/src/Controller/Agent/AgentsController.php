9583c321cf0256fd967ab6aba139eb0ab81d5761
Jacob Friedman <jacob@subsumo.com>
2015-07-22 15:02:23 -0400

    unsubscribe remove

--- envoynet/src/Controller/Agent/AgentsController.php
+++ envoynet/src/Controller/Agent/AgentsController.php
@@ -541,7 +541,7 @@ class AgentsController extends \App\Controller\AgentsController {
               } else {
                 $addAttr = "";
               }
-              $unSubBoxesTop = $unSubBoxesTop . "
+              $unSubBoxesTop = "
                 <span class='hoverImgHome' id='$subs'>
                   <input name='subscribeBoxes[]' id='sub$subs' type='checkbox' value='$subs' $addAttr/>
                   <strong><label for='sub$subs'><span style='font-size:12px;'>$nonSubName</span> - <span style='font-size:10px;font-family: SourceSansPro-Regular, Helvetica, \"Trebuchet MS\", Arial, sans-serif;'>$description_en</span></label></strong></span>
