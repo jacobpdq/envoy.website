d410cc59091cff19248b52d10f4eeaccdf8bd16f
Jacob Friedman <jacob@subsumo.com>
2015-07-22 15:04:56 -0400

    styling

--- envoynet/src/Controller/Agent/AgentsController.php
+++ envoynet/src/Controller/Agent/AgentsController.php
@@ -545,7 +545,7 @@ class AgentsController extends \App\Controller\AgentsController {
               }
               $unSubBoxesTop = $unSubBoxesTop . "
                 <span class='hoverImgHome' id='$subs'>
-                  <input style='float:left;margin-right:5px;' name='subscribeBoxes[]' id='sub$subs' type='checkbox' value='$subs' $addAttr/>
+                  <input style='float:left;margin-right:5px;margin-top:4px;' name='subscribeBoxes[]' id='sub$subs' type='checkbox' value='$subs' $addAttr/>
                   <strong><label for='sub$subs'><span style='font-size:12px;'>$nonSubName</span> - <span style='font-size:10px;font-family: SourceSansPro-Regular, Helvetica, \"Trebuchet MS\", Arial, sans-serif;'>$description_en</span></label></strong></span>
                 </span>";
             }
