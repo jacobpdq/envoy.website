f413dc92d23b842f2b64ce3fa5eacd26b5dd3a13
Glen Harwood <gharwood@envoynetworks.ca>
2015-08-13 16:34:33 -0400

    set order item status to 1 - pending for all orders received through supplier page

--- envoynet/src/Controller/Supplier/OrdersController.php
+++ envoynet/src/Controller/Supplier/OrdersController.php
@@ -240,10 +240,10 @@ class OrdersController extends \App\Controller\OrdersController {
               $orderItem->brochure_name = $brochure['name'];
               $orderItem->ontario = $brochure['ontario'];
 
-            	if ($brochure['poa'] == '1') {  //set item initial status - no need to get poa (supplier order approval from supplier orders)
+            	if ($brochure['poa'] == '1') {  //set item initial status to 1 - no need to get poa (supplier order approval from supplier orders)
                 $orderItem->status = '1';
               } else {
-                $orderItem->status = '0';
+                $orderItem->status = '1';
               }
 
               // to avoid DB changes hard coding default for now
