1e93eab6eb016288798a685c9e2620edbd814fa3
Glen Harwood <gharwood@envoynetworks.ca>
2015-08-28 21:33:05 -0400

    test state again

--- envoynet/src/Controller/Agent/AgentsController.php
+++ envoynet/src/Controller/Agent/AgentsController.php
@@ -185,8 +185,7 @@ class AgentsController extends \App\Controller\AgentsController {
           'prince edward isalnd' => 'PE', 
           'quebec' => 'QC',
           'saskatchewan' => 'SK',
-          'yukon territory' => 'YT',
-		  'alabama'=>'AL'
+          'yukon territory' => 'YT'
         ];
         if (substr($broker_url,0,7) == 'http://') {
             $broker_url = substr($broker_url,7);
