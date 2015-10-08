7afd8d0fd4efd9bfc6c9e72c532be676ba4a1af4
Glen Harwood <gharwood@envoynetworks.ca>
2015-10-08 14:44:09 -0400

    view2 test

--- envoynet/src/Controller/Admin/BrochuresController.php
+++ envoynet/src/Controller/Admin/BrochuresController.php
@@ -44,7 +44,7 @@ class BrochuresController extends \App\Controller\BrochuresController {
 
     $this->set(compact('skufind'));
 	echo $skufind;
-	$id = 1550;
+//	$id = 1550;
 //    $id=$skufind->first()->id;
 //    $this->set(compact('id'));
    
